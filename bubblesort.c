bb83c7f9cdc728926a9a78b9f3bc6e8994e93436
vaishu1205 <119730123+vaishu1205@users.noreply.github.com>
2023-05-05 12:11:24 +0530

    Add files via upload

---
+++ bubblesort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,s;
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n-1;i++)
+	{
+		for(j=0;j<n-1;j++)// or we can use for(j=0;j<n-1-i;j++)
+		{
+			if(a[j] > a[j+1])
+			{
+				s=a[j];
+				a[j]=a[j+1];
+				a[j+1]=s;
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+}
