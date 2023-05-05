d5f9602a3f15790af75e473cdab88c8f69996277
vaishu1205 <119730123+vaishu1205@users.noreply.github.com>
2023-05-05 12:09:44 +0530

    Add files via upload

---
+++ array odd from even.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int n,i;
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<=n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	int count=0;
+	for(i=0;i<=n;i++)
+	{
+		if(a[i]%2!=0)
+		{
+			printf("%d ",count++);
+		}
+	
+	}
+
+}
